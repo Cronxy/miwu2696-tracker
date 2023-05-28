7b30768b746d9aefa73e078124b9a867e72aea51
Zelda <miwu2696@uni.sydney.edu.au>
2023-05-28 18:06:35 +1000

    local storage and set up list section

--- public/formScript.js
+++ public/formScript.js
@@ -45,15 +45,81 @@ document.getElementById("artistName").addEventListener("input", (event) => {
         userComment: document.getElementById("userComment").value,
     };
 
+      // Save album to local storage
+    let albums = localStorage.getItem("albums");
+    if (albums) {
+        albums = JSON.parse(albums);
+    } else {
+        albums = [];
+    }
+    albums.push(album);
+    localStorage.setItem("albums", JSON.stringify(albums));
     console.log(album);
 
     // Success message
     alert("Album successfully added!");
 
-    // Clear form
+    // Clear form after user submitting
     document.getElementById("newAlbumForm").reset();
 });
-  
+
+// Load albums from local storage when page is loaded
+window.addEventListener("DOMContentLoaded", (event) => {
+  let albums = localStorage.getItem("albums");
+  if (albums) {
+      albums = JSON.parse(albums);
+      let albumList = document.getElementById("albumList");
+      for (let i = 0; i < albums.length; i++) {
+          let albumDiv = document.createElement("div");
+          albumDiv.textContent = "Album: " + albums[i].name + ", Artist: " + albums[i].artist;
+          albumList.appendChild(albumDiv);
+      }
+  }
+});
+
+// album list card content
+window.addEventListener("DOMContentLoaded", (event) => {
+  let albums = localStorage.getItem("albums");
+  if (albums) {
+      albums = JSON.parse(albums);
+      let albumList = document.getElementById("albumList");
+      for (let i = 0; i < albums.length; i++) {
+          let albumDiv = document.createElement("div");
+          albumDiv.className = "album-card";
+
+          let coverImg = document.createElement("img");
+          coverImg.src = albums[i].imageURL;
+          albumDiv.appendChild(coverImg);
+
+          let container = document.createElement("div");
+          container.className = "container";
+
+          let titleH2 = document.createElement("h2");
+          titleH2.textContent = albums[i].name;
+          container.appendChild(titleH2);
+
+          let artistP = document.createElement("p");
+          artistP.textContent = "Artist: " + albums[i].artist;
+          container.appendChild(artistP);
+
+          let genreP = document.createElement("p");
+          genreP.textContent = "Genre: " + albums[i].genre;
+          container.appendChild(genreP);
+
+          let yearP = document.createElement("p");
+          yearP.textContent = "Release Year: " + albums[i].releaseYear;
+          container.appendChild(yearP);
+
+          let ratingP = document.createElement("p");
+          ratingP.textContent = "Rating: " + albums[i].userRating;
+          container.appendChild(ratingP);
+
+          albumDiv.appendChild(container);
+          albumList.appendChild(albumDiv);
+      }
+  }
+});
+
   // MusicBrainz API settings for auto-generating album/artist lists
   function searchMusicBrainz(entity, query) {
     fetch(`https://musicbrainz.org/ws/2/${entity}?query=${query}&fmt=json`)
